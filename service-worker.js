1945663866eca83eac088322c52da55e7b2f3004
JoBess7 <JOBES10@ulaval.ca>
2022-05-01 14:52:51 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.095344b83dea098f84c1498564bff953.js"
+  "./precache-manifest.419ce41ff82de88edf21d67b5802da12.js"
 );
 
 self.addEventListener('message', (event) => {
