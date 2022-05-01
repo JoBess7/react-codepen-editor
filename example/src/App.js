8bfc77d6cd9a60b9f2c65f53e9a3270d25d0d1ac
JoBess7 <JOBES10@ulaval.ca>
2022-05-01 00:21:29 -0400

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-codepen-editor'
+import 'react-codepen-editor/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
