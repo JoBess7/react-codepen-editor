3be0f298886dfe7a75c21b056b0b3dc388631f38
JoBess7 <JOBES10@ulaval.ca>
2022-05-01 14:14:17 -0400

    Updates

---
+++ precache-manifest.6aca7ba76dfb8fb0a688bd74cbee751b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ec9213a219dc360848a1a5c2ce8f10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ff48d0d6f0ebc769b37",
+    "url": "./static/css/main.1f0b34ba.chunk.css"
+  },
+  {
+    "revision": "1853bd424d21280dd024",
+    "url": "./static/js/2.e86bda4e.chunk.js"
+  },
+  {
+    "revision": "df4964ae36e0e724ce02a12cce603843",
+    "url": "./static/js/2.e86bda4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff48d0d6f0ebc769b37",
+    "url": "./static/js/main.ed808d14.chunk.js"
+  },
+  {
+    "revision": "9550e79be186fd168212",
+    "url": "./static/js/runtime-main.47e5b45a.js"
+  }
+]);
